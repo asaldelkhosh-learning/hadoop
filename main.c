b55b428a457bac55263e518fabc9751be2c0f845
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-02 10:33:28 +0330

    add: close channel method

--- main.c
+++ main.c
@@ -14,11 +14,23 @@ void sendDataOverChannel(int fd, char *data)
     write(fd, data, strlen(data));
 }
 
+void getDataFromChannel(int fd, char *data)
+{
+    read(fd, data, 100);
+}
+
 int createChannel()
 {
     int fd[2];
+
     pipe(fd);
+
     return fd[0];
+}
+
+void closechannel(int fd)
+{
+    close(fd);
 }
 
 int main()
