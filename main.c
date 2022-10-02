532f935b676c85c2479f8d886439b0e1d4f7ab6e
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-02 10:32:11 +0330

    add: channel initialize

--- main.c
+++ main.c
@@ -9,6 +9,18 @@ void createProcess(char *cmd)
     }
 }
 
+void sendDataOverChannel(int fd, char *data)
+{
+    write(fd, data, strlen(data));
+}
+
+int createChannel()
+{
+    int fd[2];
+    pipe(fd);
+    return fd[0];
+}
+
 int main()
 {
     createProcess("ls");
